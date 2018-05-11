41a56d811e76b1830e33b90c5e496b31d0440697
Jannatul Abeda <abeda.cse.du@gmail.com>
2018-05-11 16:18:28 +0900

    Sample commit

---
+++ Parent/Parent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parent
+//
+//  Created by Jannatul Abeda on 2018/05/11.
+//  Copyright © 2018 Jannatul Abeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Parent/Parent/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Parent
+//
+//  Created by Jannatul Abeda on 2018/05/11.
+//  Copyright © 2018 Jannatul Abeda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
